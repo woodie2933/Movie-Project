94768b98cf9ac822b8656fde309cb226847dcd7f
도니 김 <kdh4847@naver.com>
2025-01-15 00:56:36 +0900

    가능

--- moviedata.js
+++ moviedata.js
@@ -6,7 +6,6 @@ const popular =
   "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1";
 const banner =
   "https://api.themoviedb.org/3/movie/upcoming?language=ko-KR&page=1";
-const genreAPI = "https://api.themoviedb.org/3/genre/movie/list?language=ko";
 
 // Movie API
 const fetchMovies = async function (url) {
@@ -62,96 +61,42 @@ const recentMovie = async function () {
 
 recentMovie();
 
-// 장르
-
 // recent 모달창
-// const recentModal = async function () {
-//   const data = await fetchMovies(popular);
-//   const nowModal = document.querySelector("#recent_modal");
-
-//   for (let i = 0; i < 20; i++) {
-//     const card = document.querySelector(`#card_${i}`);
-//     card.addEventListener("click", () => {
-//       const now = data[i];
-//       const recentModalVote = now.vote_average.toFixed(1);
-//       const img = now.backdrop_path;
-
-//       document.body.style.overflow = "hidden";
-//       nowModal.classList.remove("hide");
-//       nowModal.innerHTML = `
-//         <div class = "recent_modal_wrap" style = "background-image: url('https://image.tmdb.org/t/p/original/${img}')" >
-//           <i id = "close_modal" class="fa-solid fa-xmark"></i>
-//           <div class = "recent_modal_desc">
-//             <p class="recent_modal_star">RATING${recentModalVote}/10</p>
-//             <p class="recent_modal_genre">${now.genre_ids}</p>
-//             <p class="recent_modal_title">${now.title}</p>
-//             <p class="recent_modal_original_title">${now.original_title}</p>
-//             <p class="recent_modal_date">${now.release_date}</p>
-//             <p class="recent_modal_summary">${now.overview}</p>
-//           </div>
-//         </div>`;
-
-//       // 모달 닫기
-//       const closeModal = document.querySelector("#close_modal");
-//       closeModal.addEventListener("click", () => {
-//         nowModal.classList.add("hide");
-//         document.body.style.overflow = "auto";
-//         nowModal.innerHTML = "";
-//       });
-//     });
-//   }
-// };
-
-// recentModal();
-
 const recentModal = async function () {
   const data = await fetchMovies(popular);
+
   const nowModal = document.querySelector("#recent_modal");
 
-  function showModal(movie) {
-    const recentModalVote = movie.vote_average.toFixed(1);
-    const img = movie.backdrop_path;
-
-    document.body.style.overflow = "hidden";
-    nowModal.classList.remove("hide"); // 모달 항상 표시
-
-    nowModal.innerHTML = `
-      <div class="recent_modal_wrap" style="background-image: url('https://image.tmdb.org/t/p/original/${img}')">
-        <div class="shadow">
-          <i id="close_modal" class="fa-solid fa-xmark"></i>
-          <div class="recent_modal_desc">
-            <p class="recent_modal_star">RATING <span style = "font-size: 20px; color: #FFEB46">${recentModalVote}</span> / 10</p>
-            <p class="recent_modal_genre">${movie.genre_ids}</p>
-            <p class="recent_modal_title">${movie.title}</p>
-            <p class="recent_modal_original_title">${movie.original_title}</p>
-            <p class="recent_modal_date">${movie.release_date}</p>
-            <p class="recent_modal_summary">${movie.overview}</p>
-          </div>
-        </div>
-      </div>`;
-
-    // 닫기 버튼 이벤트 추가
-    const closeModal = document.querySelector("#close_modal");
-    closeModal.addEventListener("click", () => {
-      nowModal.classList.add("hide");
-      document.body.style.overflow = "auto";
-      nowModal.innerHTML = "";
-    });
-  }
-
-  // **초기 실행 시 첫 번째 영화 자동 표시**
-  // if (data.length > 0) {
-  //   showModal(data[0]);
-  // }
-
-  // 각 카드 클릭 시 모달 업데이트
   for (let i = 0; i < 20; i++) {
     const card = document.querySelector(`#card_${i}`);
-    if (card) {
-      card.addEventListener("click", () => {
-        showModal(data[i]);
+    card.addEventListener("click", () => {
+      const top = data[i];
+      const nowModalVote = top.vote_average.toFixed(1);
+      const img = top.backdrop_path;
+
+      document.body.style.overflow = "hidden";
+      nowModal.classList.remove("hide");
+      nowModal.innerHTML += `
+        <div class = "top_rated_modal_wrap" style = "background-image: url('https://image.tmdb.org/t/p/original/${img}')" >
+          <i id = "close_modal2" class="fa-solid fa-xmark"></i>
+          <div class = "top_rated_modal_desc">
+            <p class="top_rated_modal_star">RATING${nowModalVote}/10</p>
+            <p class="top_rated_modal_genre">${top.genre_ids}</p>
+            <p class="top_rated_modal_title">${top.title}</p>
+            <p class="top_rated_modal_original_title">${top.original_title}</p>
+            <p class="top_rated_modal_date">${top.release_date}</p>
+            <p class="top_rated_modal_summary">${top.overview}</p>
+          </div>
+        </div>`;
+
+      // 모달 닫기
+      const closeModal = document.querySelector("#close_modal2");
+      closeModal.addEventListener("click", () => {
+        nowModal.classList.add("hide");
+        document.body.style.overflow = "auto";
+        nowModal.innerHTML = "";
       });
-    }
+    });
   }
 };
 
--- script.js
+++ script.js
@@ -1,25 +1,191 @@
-// const url =
-// "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1";
+// // topRated 모달창
+// const topRatedModal = async function () {
+//   const data = await fetchMovies(topRated);
 
-// try {
-// const topRatedRes = await fetch(url, options);
-// const topRatedJson = await topRatedRes.json();
-// const topRatedData = topRatedJson.results;
+//   const topModal = document.querySelector("#top_rated_modal");
 
-// renderMovie(topRatedData);
-// } catch (err) {
-// console.error("error", error);
-// }
+//   for (let i = 0; i < 20; i++) {
+//     const card2 = document.querySelector(`#card2_${i}`);
+//     card2.addEventListener("click", () => {
+//       const top = data[i];
+//       const topModalVote = top.vote_average.toFixed(1);
+//       const img = top.backdrop_path;
+
+//       document.body.style.overflow = "hidden";
+//       topModal.classList.remove("hide");
+//       topModal.innerHTML += `
+//         <div class = "top_rated_modal_wrap" style = "background-image: url('https://image.tmdb.org/t/p/original/${img}')" >
+//           <i id = "close_modal2" class="fa-solid fa-xmark"></i>
+//           <div class = "top_rated_modal_desc">
+//             <p class="top_rated_modal_star">RATING${topModalVote}/10</p>
+//             <p class="top_rated_modal_genre">${top.genre_ids}</p>
+//             <p class="top_rated_modal_title">${top.title}</p>
+//             <p class="top_rated_modal_original_title">${top.original_title}</p>
+//             <p class="top_rated_modal_date">${top.release_date}</p>
+//             <p class="top_rated_modal_summary">${top.overview}</p>
+//           </div>
+//         </div>`;
+
+//       // 모달 닫기
+//       const closeModal = document.querySelector("#close_modal2");
+//       closeModal.addEventListener("click", () => {
+//         topModal.classList.add("hide");
+//         document.body.style.overflow = "auto";
+//         topModal.innerHTML = "";
+//       });
+//     });
+//   }
 // };
 
-// const renderMovie = function (data) {
-// const movie = document.getElementById("movies");
-// data.forEach((item) => {
-// movie.innerHTML += `<h3 class>${item.title}</h3>
-// <p>${item.vote_average}</p>
-// <img class="img" src ='https://image.tmdb.org/t/p/w300/${item.poster_path}'>`;
-// });
-// console.log(data);
+// topRatedModal();
+
+// const recentModal = async function () {
+//   const data = await fetchMovies(popular);
+//   const nowModal = document.querySelector("#recent_modal");
+
+//   function showModal(movie) {
+//     const recentModalVote = movie.vote_average.toFixed(1);
+//     const img = movie.backdrop_path;
+
+//     document.body.style.overflow = "hidden";
+//     nowModal.classList.remove("hide"); // 모달 항상 표시
+
+//     nowModal.innerHTML = `
+//       <div class="recent_modal_wrap" style="background-image: url('https://image.tmdb.org/t/p/original/${img}')">
+//         <div class="shadow">
+//           <i id="close_modal" class="fa-solid fa-xmark"></i>
+//           <div class="recent_modal_desc">
+//             <p class="recent_modal_star">RATING <span style = "font-size: 20px; color: #FFEB46">${recentModalVote}</span> / 10</p>
+//             <p class="recent_modal_genre">${movie.genre_ids}</p>
+//             <p class="recent_modal_title">${movie.title}</p>
+//             <p class="recent_modal_original_title">${movie.original_title}</p>
+//             <p class="recent_modal_date">${movie.release_date}</p>
+//             <p class="recent_modal_summary">${movie.overview}</p>
+//           </div>
+//         </div>
+//       </div>`;
+
+//     // 닫기 버튼 이벤트 추가
+//     const closeModal = document.querySelector("#close_modal");
+//     closeModal.addEventListener("click", () => {
+//       nowModal.classList.add("hide");
+//       document.body.style.overflow = "auto";
+//       nowModal.innerHTML = "";
+//     });
+//   }
+
+//   // **초기 실행 시 첫 번째 영화 자동 표시**
+//   // if (data.length > 0) {
+//   //   showModal(data[0]);
+//   // }
+
+//   // 각 카드 클릭 시 모달 업데이트
+//   for (let i = 0; i < 20; i++) {
+//     const card = document.querySelector(`#card_${i}`);
+//     if (card) {
+//       card.addEventListener("click", () => {
+//         showModal(data[i]);
+//       });
+//     }
+//   }
 // };
 
-// getMovies();
+// recentModal();
+
+// recent 모달창
+// const recentModal = async function () {
+//   const data = await fetchMovies(popular);
+//   const nowModal = document.querySelector("#recent_modal");
+
+//   for (let i = 0; i < 20; i++) {
+//     const card = document.querySelector(`#card_${i}`);
+//     card.addEventListener("click", () => {
+//       const now = data[i];
+//       const recentModalVote = now.vote_average.toFixed(1);
+//       const img = now.backdrop_path;
+
+//       document.body.style.overflow = "hidden";
+//       nowModal.classList.remove("hide");
+//       nowModal.innerHTML += `
+//         <div class = "recent_modal_wrap" style = "background-image: url('https://image.tmdb.org/t/p/original/${img}')" >
+//           <i id = "close_modal" class="fa-solid fa-xmark"></i>
+//           <div class = "recent_modal_desc">
+//             <p class="recent_modal_star">RATING${recentModalVote}/10</p>
+//             <p class="recent_modal_genre">${now.genre_ids}</p>
+//             <p class="recent_modal_title">${now.title}</p>
+//             <p class="recent_modal_original_title">${now.original_title}</p>
+//             <p class="recent_modal_date">${now.release_date}</p>
+//             <p class="recent_modal_summary">${now.overview}</p>
+//           </div>
+//         </div>`;
+
+//       // 모달 닫기
+//       const closeModal = document.querySelector("#close_modal");
+//       closeModal.addEventListener("click", () => {
+//         nowModal.classList.add("hide");
+//         document.body.style.overflow = "auto";
+//         nowModal.innerHTML = "";
+//       });
+//     });
+//   }
+// };
+
+// recentModal();
+
+// recent 모달창
+// const recentModal = async function () {
+//   const data = await fetchMovies(popular);
+//   const nowModal = document.querySelector("#recent_modal");
+
+//   function showModal(movie) {
+//     const recentModalVote = movie.vote_average.toFixed(1);
+//     const img = movie.backdrop_path;
+
+//     document.body.style.overflow = "hidden";
+//     nowModal.classList.remove("hide"); // 모달 항상 표시
+
+//     nowModal.innerHTML = `
+//       <div class="recent_modal_wrap" style="background-image: url('https://image.tmdb.org/t/p/original/${img}')">
+//         <div class="shadow">
+//           <i id="close_modal" class="fa-solid fa-xmark"></i>
+//           <div class="recent_modal_desc">
+//             <p class="recent_modal_star">RATING <span style = "font-size: 20px; color: #FFEB46">${recentModalVote}</span> / 10</p>
+//             <p class="recent_modal_genre">${movie.genre_ids}</p>
+//             <p class="recent_modal_title">${movie.title}</p>
+//             <p class="recent_modal_original_title">${movie.original_title}</p>
+//             <p class="recent_modal_date">${movie.release_date}</p>
+//             <p class="recent_modal_summary">${movie.overview}</p>
+//           </div>
+//         </div>
+//       </div>`;
+
+//     // 닫기 버튼 이벤트 추가
+//     serTimeout(() => {
+//       const closeModal = document.querySelector("#close_modal");
+//       if (closeModal) {
+//         closeModal.addEventListener("click", () => {
+//           nowModal.classList.add("hide");
+//           document.body.style.overflow = "auto";
+//           nowModal.innerHTML = "";
+//         });
+//       }
+//     }, 0);
+//   }
+
+//   // **초기 실행 시 첫 번째 영화 자동 표시**
+//   if (data.length > 0) {
+//     showModal(data[0]);
+//   }
+
+//   // 각 카드 클릭 시 모달 업데이트
+//   for (let i = 0; i < 20; i++) {
+//     const card = document.querySelector(`#card_${i}`);
+//     if (card) {
+//       card.addEventListener("click", () => {
+//         showModal(data[i]);
+//       });
+//     }
+//   }
+// };
+
+// recentModal();
